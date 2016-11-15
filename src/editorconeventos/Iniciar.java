b85936aed495e81ae93e930339898cb7c69db5bd
loxfalcon <luis0079@gmail.com>
2016-11-15 00:03:10 -0600

    Quité la clase de Generador de Código Intermedio

--- src/editorconeventos/Iniciar.java
+++ src/editorconeventos/Iniciar.java
@@ -6,7 +6,6 @@
 
 package editorconeventos;
 
-import analizador.GeneradorCodigoIntermedio;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.io.UnsupportedEncodingException;
@@ -26,12 +25,6 @@ public class Iniciar {
         VentanaEditor f = new VentanaEditor();
         PanelEditor p = new PanelEditor();
         PrintStream out = new PrintStream(new Consola(p.getAreaConsola()), true, "UTF-8");
-        GeneradorCodigoIntermedio gen = new GeneradorCodigoIntermedio();    
-        gen.declaracion("a", 37, "5");
-        gen.print("a", 37);
-        gen.generateCode();
-        //gen.declaracion("a", 37, "( z + 5 ) * 10 / ( 5 - 3 )");
-        //gen.printCode();
         System.setOut(out);
         OyenteEditor oyente = new OyenteEditor(p, f);
         f.setSize(800, 600);
